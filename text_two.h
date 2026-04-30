823d770efc17f02b822914939c29397b9bcc4842
zlm02 <2303669747@qq.com>
2026-04-30 22:50:39 +0800

    c++，多态实现的职工管理系统

---
+++ text_two.h
@@ -0,0 +1,13 @@
+
+#pragma once//防止头文件重复包含。
+#include <iostream>
+using namespace std;
+
+class WorkerManager {
+public:
+    WorkerManager();
+    //展示菜单
+    void Show_Menu();
+
+    ~WorkerManager();
+};
---
+++ text_two_main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+#include"text_two.h"
+
+int main() {
+    //实例化对象
+    WorkerManager wm;
+    wm.Show_Menu();
+
+    return 0;
+
+}

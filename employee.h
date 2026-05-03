3e31d5c9e5393783493a2613c8829878afdab669
zlm02 <2303669747@qq.com>
2026-05-03 22:55:49 +0800

    员工类声明

---
+++ employee.h
@@ -0,0 +1,14 @@
+//普通员工文件
+#pragma once//防止头文件重复包含。
+#include<iostream>
+using namespace std;
+#include"worker.h"
+
+class Employee:public Worker {
+public:
+    Employee(int id,string name,int dId);
+    //显示个人信息
+    virtual void showInfo();
+    //获取岗位
+    virtual string getDeptName();
+};

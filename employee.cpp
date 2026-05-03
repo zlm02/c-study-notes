20a4dc1adb9bb3c3e017f0485c320cc0e4b3ffd1
zlm02 <2303669747@qq.com>
2026-05-03 22:55:59 +0800

    员工类实现

---
+++ employee.cpp
@@ -0,0 +1,17 @@
+#include<employee.h>
+Employee::Employee(int id,string name,int dId) {
+ this->m_id=id;
+ this->m_Name=name;
+ this->m_DeptId=dId;
+}
+//显示个人信息
+ void Employee::showInfo() {
+ cout<<"职工编号："<<this->m_id
+     <<"\t职工姓名"<<this->m_Name
+     <<"\t岗位"<<this->getDeptName()
+     <<"\t岗位职责：完成上级交给的任务"<<endl;
+}
+//获取岗位
+string Employee::getDeptName() {
+    return string("员工");
+}

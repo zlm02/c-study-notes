2a36e01afba1725afe0cf1d2a2811c6187f87971
zlm02 <2303669747@qq.com>
2026-05-04 22:46:18 +0800

    普通员工类实现

--- employee.cpp
+++ employee.cpp
@@ -7,8 +7,8 @@ Employee::Employee(int id,string name,int dId) {
 //显示个人信息
  void Employee::showInfo() {
  cout<<"职工编号："<<this->m_id
-     <<"\t职工姓名"<<this->m_Name
-     <<"\t岗位"<<this->getDeptName()
+     <<"\t职工姓名:"<<this->m_Name
+     <<"\t岗位:"<<this->getDeptName()
      <<"\t岗位职责：完成上级交给的任务"<<endl;
 }
 //获取岗位

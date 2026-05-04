75f4e314012383bd7beee7b5b1352fc79c6a78c7
zlm02 <2303669747@qq.com>
2026-05-04 22:46:41 +0800

    经理类声明

---
+++ manager.h
@@ -0,0 +1,15 @@
+#pragma once//防止头文件重复包含。
+#include<iostream>
+using namespace std;
+#include"worker.h"
+
+//经理类
+class Manager:public Worker {
+public:
+    //构造函数
+    Manager(int id,string name,int dId);
+    //显示个人信息
+    virtual void showInfo();
+    //获取岗位
+    virtual string getDeptName();
+};

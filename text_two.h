ba0a756755f9a44be88e8a20d0d0657287a4e0a9
zlm02 <2303669747@qq.com>
2026-05-05 16:08:46 +0800

    员工管理系统主体声明

--- text_two.h
+++ text_two.h
@@ -3,6 +3,12 @@
 #include <iostream>
 #include <limits>
 using namespace std;
+#include"worker.h"
+#include"manager.h"
+#include"employee.h"
+#include"boss.h"
+#include<fstream>
+#define FILENAME "empFile.txt"
 
 class WorkerManager {
 public:
@@ -11,6 +17,14 @@ public:
     void Show_Menu();
     //退出
     void ExitSystem();
+    //记录职工人数
+    int m_EmpNum;
+    //职工数组指针
+    Worker **m_EmpArray;
+    //添加职工
+    void Add_Emp();
+    //保存文件
+    void save();
 
 
 
